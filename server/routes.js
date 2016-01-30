773d1b8f7cd7092b30b871394c9a1d0818dd9f5e
akshath4u <akshath@baabte.com>
2016-01-30 10:12:11 +0530

    commited changes

--- server/routes.js
+++ server/routes.js
@@ -13,8 +13,7 @@ module.exports = function(app) {
   app.use('/api/bills', require('./api/bill'));
   app.use('/api/reports', require('./api/report'));
   app.use('/api/users', require('./api/user'));
-  app.use('/api/things', require('./api/thing'));
-  
+
   // All undefined asset or api routes should return a 404
   app.route('/:url(api|auth|components|app|bower_components|assets)/*')
    .get(errors[404]);
