afaa3b4fed1d7320f7473a7363cd476f40fc57b6
akshath kumar <akshath.kumar@votercircle.com>
2016-09-25 17:40:49 +0530

    added changes in users panel

--- client/app/users/users.controller.js
+++ client/app/users/users.controller.js
@@ -2,108 +2,121 @@
 
 angular.module('meanAppApp')
   .controller('UsersCtrl', function ($scope,$modal,$http,toaster) {
-  var self=this;
+    var self=this;
 
-  self.sortType     = 'name'; // set the default sort type
-  self.sortReverse  = false;  // set the default sort order
-  self.searchUser   = '';     // set the default search/filter term
+    self.sortType     = 'name'; // set the default sort type
+    self.sortReverse  = false;  // set the default sort order
+    self.searchUser   = '';     // set the default search/filter term
 
-  //load the registered users
-  self.loadRegisteredusers = function() {
-    self.loader = true;
-    $http.get('/api/users').success(function(users) {
-      self.userList = users;
-      self.loader = false;
-    });
-  };
+    //load the registered users
+    self.loadRegisteredusers = function() {
+        self.loader = true;
+        $http.get('/api/users').success(function(users) {
+            self.userList = users;
+            self.loader = false;
+        });
+    };
 
-  self.convert=function(date){
-  	date = new Date(date);
-  };
+    self.convert=function(date){
+        date = new Date(date);
+    };
 
-  self.loadRegisteredusers();
-  self.user={address:[{}]};
-  self.selectedUser={};
-  
-  self.open=function(user,type){
+    self.loadRegisteredusers();
+    self.user={address:[{}]};
+    self.selectedUser={};
+    
+    self.open=function(user,type){
 
-    if(angular.equals(type,'edit')){
-      self.selectedUser.type='edit';
-      self.user= user;
-      self.user.dob=new Date(self.user.dob);
-    }else{
-      self.selectedUser.type='register';
+        if(angular.equals(type,'edit')){
+        self.selectedUser.type='edit';
+        self.user= user;
+        self.user.dob=new Date(self.user.dob);
+        }else{
+        self.selectedUser.type='register';
+        }
+
+        self.modalInstance=$modal.open({
+        templateUrl: '../app/users/users.modal.html',
+        scope: $scope,
+        controller: 'UsersCtrl',
+        size: 'md',
+        resolve: {
+            user: function () {
+            return self.user;
+            }
+        }
+        });
     }
 
-    self.modalInstance=$modal.open({
-    templateUrl: '../app/users/users.modal.html',
-    scope: $scope,
-      controller: 'UsersCtrl',
-      size: 'md',
-      resolve: {
-        user: function () {
-          return self.user;
+    self.register = function() {
+        self.user.role='user';
+        $http.post('/api/users', self.user).success(function(user) {
+        self.modalInstance.dismiss('cancel');
+        toaster.pop('success', 'Success', 'Succesfully Registered!');
+        self.userList.push(user);
+        self.user={};
+        }).error(function(err){
+            toaster.pop('Oops!', 'Warning', 'Something went wrong. Please try again!');
+        });
+    };
+
+    self.cancel = function() {
+        self.modalInstance.dismiss('cancel');
+        self.user={};
+    };
+
+
+    self.deleteConfirm=function(user){
+        self.user=user;
+        self.deleteModalInstance=$modal.open({
+        template: '<div class="modal-header"><h4 class="modal-title">Delete user</h4></div> <div class="modal-body">Are you sure you want to delete?</div><div class="modal-footer"><button class="btn btn-primary" type="button" ng-click="userCtrl.deleteUser()">Yes</button><button class="btn btn-primary" type="button" ng-click="userCtrl.cancelConfirm()">No</button></div>',
+        scope: $scope,
+        controller: 'UsersCtrl',
+        size: 'sm',
+        resolve: {
+            user: function () {
+            return self.user;
+            }
         }
-      }
-    });
-  }
+        });
+        
+    };
 
-  self.register = function() {
-    self.user.role='user';
-    $http.post('/api/users', self.user).success(function(user) {
-      self.modalInstance.dismiss('cancel');
-      toaster.pop('success', 'Success', 'Succesfully Registered!');
-      self.userList.push(user);
-      self.user={};
-    }).error(function(err){
-    	toaster.pop('Oops!', 'Warning', 'Something went wrong. Please try again!');
-    });
-  };
+    self.deleteUser=function(){
+        self.deleteModalInstance.dismiss('cancel');
+        $http.delete('/api/users/'+self.user._id).success(function(user) {
+        self.loadRegisteredusers();
+        toaster.pop('success', 'Success', 'Succesfully Deleted!');
+        }).error(function(err){
+            toaster.pop('Oops!', 'Warning', 'Something went wrong. Please try again!');
+        });
+    };
 
-  self.cancel = function() {
-    self.modalInstance.dismiss('cancel');
-    self.user={};
-  };
+    self.cancelConfirm=function(){
+        self.deleteModalInstance.dismiss('cancel');
+    };
 
+    self.update = function() {
+        $http.put('/api/users/'+self.user._id, self.user).success(function(user) {
+        self.modalInstance.dismiss('cancel');
+        self.user={};
+        self.loadRegisteredusers();
+        toaster.pop('success', 'Success', 'Succesfully Updated!');
+        }).error(function(err){
+            toaster.pop('Oops!', 'Warning', 'Something went wrong. Please try again!');
+        });
+    };
 
-  self.deleteConfirm=function(user){
-  	self.user=user;
-    self.deleteModalInstance=$modal.open({
-    template: '<div class="modal-header"><h4 class="modal-title">Delete user</h4></div> <div class="modal-body">Are you sure you want to delete?</div><div class="modal-footer"><button class="btn btn-primary" type="button" ng-click="userCtrl.deleteUser()">Yes</button><button class="btn btn-primary" type="button" ng-click="userCtrl.cancelConfirm()">No</button></div>',
-    scope: $scope,
-      controller: 'UsersCtrl',
-      size: 'sm',
-      resolve: {
-        user: function () {
-          return self.user;
+    self.fetchUsers = function(){
+        console.log(self.searchUser);
+        if(self.searchUser === ''){
+            self.loadRegisteredusers();
+        }else{
+            $http.get('/api/users/search/' + self.searchUser).success(function(users) {
+                self.userList = users;
+            }).error(function(err){
+                toaster.pop('Oops!', 'Warning', 'Something went wrong. Please try again!');
+            });
         }
-      }
-    });
-    
-  };
-
-  self.deleteUser=function(){
-    self.deleteModalInstance.dismiss('cancel');
-    $http.delete('/api/users/'+self.user._id).success(function(user) {
-      self.loadRegisteredusers();
-      toaster.pop('success', 'Success', 'Succesfully Deleted!');
-    }).error(function(err){
-    	toaster.pop('Oops!', 'Warning', 'Something went wrong. Please try again!');
-    });
-  };
-
-  self.cancelConfirm=function(){
-    self.deleteModalInstance.dismiss('cancel');
-  };
-
-  self.update = function() {
-    $http.put('/api/users/'+self.user._id, self.user).success(function(user) {
-      self.modalInstance.dismiss('cancel');
-      self.user={};
-      self.loadRegisteredusers();
-      toaster.pop('success', 'Success', 'Succesfully Updated!');
-    }).error(function(err){
-    	toaster.pop('Oops!', 'Warning', 'Something went wrong. Please try again!');
-    });
-  };
-  });
+    };
+});
--- server/api/user/user.controller.js
+++ server/api/user/user.controller.js
@@ -5,42 +5,51 @@ var User = require('./user.model');
 
 // Get list of users
 exports.index = function(req, res) {
-  User.fetchAll().then(function (users) {
-    return res.status(200).json(users);
-  },function(err){
-    return handleError(res, err);
-  });
+    User.fetchAll().then(function (users) {
+        return res.status(200).json(users);
+    },function(err){
+        return handleError(res, err);
+    });
 };
 
 // Get a single user
 exports.show = function(req, res) {
-  User.findById(req.params.id, function (err, user) {
-    if(err) { return handleError(res, err); }
-    if(!user) { return res.status(404).send('Not Found'); }
-    return res.json(user);
-  });
+    User.findById(req.params.id, function (err, user) {
+        if(err) { return handleError(res, err); }
+        if(!user) { return res.status(404).send('Not Found'); }
+        return res.json(user);
+    });
+};
+
+exports.search = function(req, res) {
+    var reg = new RegExp(req.params.search_text,'i');
+    User.find({$or : [{name: reg}, {email: reg}]}, function (err, users) {
+        if(err) { return handleError(res, err); }
+        if(!users) { return res.status(404).send('Not Found'); }
+        return res.json(users);
+    });
 };
 
 // Creates a new user in the DB.
 exports.create = function(req, res) {
-  User.create(req.body, function(err, user) {
-    if(err) { return handleError(res, err); }
-    return res.status(201).json(user);
-  });
+    User.create(req.body, function(err, user) {
+        if(err) { return handleError(res, err); }
+        return res.status(201).json(user);
+    });
 };
 
 // Updates an existing user in the DB.
 exports.update = function(req, res) {
-  if(req.body._id) { delete req.body._id; }
-  User.findById(req.params.id, function (err, user) {
-    if (err) { return handleError(res, err); }
-    if(!user) { return res.status(404).send('Not Found'); }
-    var updated = _.merge(user, req.body);
-    updated.save(function (err) {
-      if (err) { return handleError(res, err); }
-      return res.status(200).json(user);
+    if(req.body._id) { delete req.body._id; }
+    User.findById(req.params.id, function (err, user) {
+        if (err) { return handleError(res, err); }
+        if(!user) { return res.status(404).send('Not Found'); }
+        var updated = _.merge(user, req.body);
+        updated.save(function (err) {
+        if (err) { return handleError(res, err); }
+        return res.status(200).json(user);
+        });
     });
-  });
 };
 
 // Deletes a user from the DB.
